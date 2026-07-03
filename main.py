b9869e3d00a1ef4c849c3143409e76140e16cf1d
Felipe Moreira da Cruz <piradous@gmail.com>
2026-07-03 16:31:18 +0000

    mensagem

--- main.py
+++ main.py
@@ -1,7 +1,33 @@
 from fastapi import FastAPI
+from datetime import datetime
 
+
+LISTA_TAREFAS = []
 APP = FastAPI()
 
+def nova_tarefa(id: int, titulo: str, descricao: str, concluido: bool):
+    return {
+        "id": id,
+        "titulo": titulo,
+        "descricao": descricao,
+        "concluido": False,
+        "criado_em": datetime.now()
+    }
+
 @APP.get("/")
-def index()
+def index():
     return "Olá, DevOps!"
+
+@APP.get("/tarefas")
+def listar_tarefas():
+    if len(LISTA_TAREFAS) == 0:
+        return LISTA_TAREFAS
+    
+    tarefas = []
+
+    for tarefa in LISTA_TAREFAS:
+        info = {"id": tarefa['id'], "titulo": taerfa['titulo']}
+        tarefas.append(info)
+        
+    return tarefas
+    
