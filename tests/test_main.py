8a50b0c1928527f16d5d34f98598d07f79c01ae2
Felipe Moreira da Cruz <piradous@gmail.com>
2026-07-04 14:16:59 +0000

    Adicionado testes para main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,8 +1,38 @@
-def soma(a, b):
-    return a + b
+from fastapi.testclient import TestClient
 
-def test_soma_positivo():
-    assert soma(4, 4) == 8
+from app import APP
 
-def test_soma_errada():
-    assert soma(3, 7) == 10
+CLIENT = TestClient(APP)
+
+def test_index():
+    requisicao = CLIENT.get("/")
+
+    assert requisicao.status_code == 200
+    assert requisicao.json() == "Olá, DevOps!"
+
+def test_criar_tarefa_com_sucesso():
+    dados_tarefa = {
+        "id": 1,
+        "titulo": "Aprender Testes",
+        "descricao": "Criar testes unitários com Pytest e FastAPI"
+    }
+
+    requisicao = CLIENT.post("/tarefas", params=dados_tarefa)
+
+    assert requisicao.status_code == 200
+    assert requisicao.json() == {"mensagem": "OK"}
+
+def test_criar_tarefa_ja_existente():
+    dados_tarefa = {
+        "id": 2,
+        "titulo": "Tarefa Duplicada",
+        "descricao": "Testando a duplicidade"
+    }
+
+    CLIENT.post("/tarefas", params=dados_tarefa)
+
+    requisicao = CLIENT.post("/tarefas", params=dados_tarefa)
+
+    assert requisicao.status_code == 200
+    assert requisicao.json() == {"mensagem": "TAREFA JÁ EXISTE"}
+
