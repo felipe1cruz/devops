03a1879f1ca150ad2b79a8f204c45106bf29666c
Felipe Moreira da Cruz <piradous@gmail.com>
2026-07-23 22:24:13 +0000

    fix: notificacao.py

--- app/notificacao.py
+++ app/notificacao.py
@@ -1,17 +1,28 @@
 from fastapi import FastAPI
+from datetime import datetime
 
 APP_NOTIFICACAO = FastAPI()
-    
+
 # Criar uma rota para receber tarefa finalizada
 # APP_NOTIFICACAO.post("/notificar")
 # Entrada:
 #   - Recebe título da tarefa e data de finalização da tarefa
 # Saída:
 #   - print no terminal
+
+NOTIFICACOES = []
+
+@APP_NOTIFICACAO.get("/notificar")
+def listar_notificacoes():
+    return NOTIFICACOES
+
 @APP_NOTIFICACAO.post("/notificar")
-def notificar_tarefa_finalizada(titulo: str, data_finalizacao: str):
-    print(f"Tarefa finalizada: {titulo}, Data de finalização: {data_finalizacao}")
-    return {"mensagem": "Notificação recebida com sucesso!"}
+def notificar(titulo: str, data_finalizacao: datetime):
+    global NOTIFICACOES
+    
+    resultado = f"Tarefa '{titulo}' finalizada em {data_finalizacao}"
+    print(resultado)
 
+    NOTIFICACOES.append(resultado)
 
-
+    return {"status": "OK"}
